d173da9d521d412f622a872dad9ff44dfe65f32e
PrathikshaNayak <prathikshanayak224@gmail.com>
2025-11-14 01:02:50 +0530

    Update build.gradle.kts with OpenCV dependencies and plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,27 +30,33 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
         }
     }
+
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
+    // AndroidX and testing libraries
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // OpenCV
+    implementation("org.opencv:opencv-android:4.7.0")
 }
