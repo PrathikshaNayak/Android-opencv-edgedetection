034046f42261d2bddb1ae32cd01f952a3953465b
PrathikshaNayak <prathikshanayak224@gmail.com>
2025-11-14 01:02:43 +0530

    Add native-lib.cpp with native methods for edge detection

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,24 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/opencv.hpp>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_assessment_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF("OpenCV Loaded Successfully!");
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_example_assessment_MainActivity_edgeDetection(
+        JNIEnv* env,
+        jobject /* this */,
+        jlong matAddrInput,
+        jlong matAddrOutput) {
+
+    cv::Mat &input = *(cv::Mat *) matAddrInput;
+    cv::Mat &output = *(cv::Mat *) matAddrOutput;
+
+    cv::cvtColor(input, output, cv::COLOR_RGBA2GRAY);
+    cv::Canny(output, output, 80, 100);
 }
