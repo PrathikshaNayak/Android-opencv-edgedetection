f8a4cfc19039519e6c70c79e41180d7e3a6eabe6
PrathikshaNayak <prathikshanayak224@gmail.com>
2025-11-14 14:48:24 +0530

    Add clean UI and update MainActivity for image picker structure

--- app/src/main/java/com/example/assessment/MainActivity.java
+++ app/src/main/java/com/example/assessment/MainActivity.java
@@ -1,18 +1,31 @@
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 package com.example.assessment;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.net.Uri;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import org.opencv.android.OpenCVLoader;
 
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView inputImageView, outputImageView;
     Button pickImageBtn, detectEdgesBtn;
+    Bitmap selectedBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,33 @@ public class MainActivity extends AppCompatActivity {
             Log.e("OpenCV", "OpenCV initialization FAILED");
         } else {
             Log.d("OpenCV", "OpenCV initialized SUCCESSFULLY");
+        }
+
+        // Pick image button
+        pickImageBtn.setOnClickListener(v -> openImagePicker());
+    }
+
+    // Image Picker launcher
+    ActivityResultLauncher<String> imagePickerLauncher =
+            registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
+                if (uri != null) {
+                    loadImage(uri);
+                }
+            });
+
+    // Opens the gallery
+    private void openImagePicker() {
+        imagePickerLauncher.launch("image/*");
+    }
+
+    // Loads selected image into ImageView
+    private void loadImage(Uri uri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            selectedBitmap = BitmapFactory.decodeStream(inputStream);
+            inputImageView.setImageBitmap(selectedBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
